7ac2a43241b9cc3ee594a502e8f8b555df870f99
Akbari06 <akbarishtiaque@gmail.com>
2025-12-29 14:02:26 +0000

    added locks to allow one thread at a time access to balance++

--- threads-api/main-race.c
+++ threads-api/main-race.c
@@ -2,16 +2,27 @@
 
 #include "common_threads.h"
 
+// The data race from the report on the shared variable balance is reported and correctly points to 2 balance++ lines in main and worker. It also shows the accesses are writes fom different threads, provides stack traces for each access and indicates no lock is held during the updates.
+
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+
 int balance = 0;
 
 void* worker(void* arg) {
-    balance++; // unprotected access 
+    pthread_mutex_lock(&m);
+    balance++;
+    pthread_mutex_unlock(&m);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t p;
     Pthread_create(&p, NULL, worker, NULL);
+
+    pthread_mutex_lock(&m);
+    balance++;
+    pthread_mutex_unlock(&m);
+    
     Pthread_join(p, NULL);
     return 0;
 }
